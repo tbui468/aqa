7949d33ac882a5922bb30c49e5ef0b153c7fa54f
Thomas <thomasthienbui@gmail.com>
2021-03-16 15:21:41 +0700

    Added longer text to answers in populate.sql

--- db/populate.sql
+++ db/populate.sql
@@ -1,5 +1,7 @@
 \i ~/Dev/aqa/db/createTables.sql;
 
+\set lorem_ipsum 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum viverra bibendum mauris ac consectetur. Integer in risus suscipit, aliquam est sit amet, molestie risus. Nunc et velit et arcu iaculis vulputate. Sed accumsan eget dui vel pellentesque. Maecenas sit amet lacus felis. Integer risus nisl, dictum vitae luctus eget, lacinia vitae est. Etiam at posuere ex.Nam in purus sit amet odio ornare pellentesque ultricies et ex. Nam nec rutrum mauris, eget condimentum sem. Curabitur erat curae.'
+
 
 /* add dummy user data - note: passwords won't be properly hashed, so login will be impossible */
 INSERT INTO users 
@@ -32,18 +34,18 @@ VALUES
     ('This is a law/governmetn question', 'Law and Government', current_timestamp, (SELECT user_id FROM users WHERE user_name='Abel')),
     ('This is a business question', 'Business and Administration', current_timestamp, (SELECT user_id FROM users WHERE user_name='Abel'));
 
-/* add dummy answer data - Bob's answers */
+    /* add dummy answer data - Bob's answers */
 INSERT INTO answers
     (answer_text, answer_date, answer_user, answer_question) 
 VALUES
-    ('Very.  Do not take any risks', current_timestamp, 2, (SELECT question_id FROM questions WHERE question_id=1)),
-    ('He known as a killer to his enemies, but a great leader to many others', current_timestamp, 2, (SELECT question_id FROM questions WHERE question_id=2)),
+    (:'lorem_ipsum', current_timestamp, 2, (SELECT question_id FROM questions WHERE question_id=1)),
+    (:'lorem_ipsum', current_timestamp, 2, (SELECT question_id FROM questions WHERE question_id=2)),
 
-/* add dummy answer data - Catherine's answers */
-    ('Worst than regular flu, but not as bad as 1918 one.', current_timestamp, 3, (SELECT question_id FROM questions WHERE question_id=1)),
-    ('No, he was a terrible leader.  And a rapist', current_timestamp, 3, (SELECT question_id FROM questions WHERE question_id=2)),
+    /* add dummy answer data - Catherine's answers */
+    (:'lorem_ipsum', current_timestamp, 3, (SELECT question_id FROM questions WHERE question_id=1)),
+    (:'lorem_ipsum', current_timestamp, 3, (SELECT question_id FROM questions WHERE question_id=2)),
 
-/* Grant and Heather's answers to weight testing questions: Bob and Catherine will vote on these */
+    /* Grant and Heather's answers to weight testing questions: Bob and Catherine will vote on these */
     ('This Grants answer to the medical question.', current_timestamp, (SELECT user_id FROM users WHERE user_name='Grant'), (SELECT question_id FROM questions WHERE question_id=3)),
     ('This Heathers answer to the medical question.', current_timestamp, (SELECT user_id FROM users WHERE user_name='Heather'), (SELECT question_id FROM questions WHERE question_id=3)),
     ('This Grants answer to the history question.', current_timestamp, (SELECT user_id FROM users WHERE user_name='Grant'), (SELECT question_id FROM questions WHERE question_id=4)),
@@ -52,11 +54,11 @@ VALUES
     ('This Heathers answer to the business question.', current_timestamp, (SELECT user_id FROM users WHERE user_name='Heather'), (SELECT question_id FROM questions WHERE question_id=5));
 
 
-/* add dummy votes data - Darcie, Eugene and Fortraine vote for one answer for each of the three questions */
-/* answer 1 and 2 are Bob's (medical and history), 3 and 4 are Catherine's (medical and history) */
-/* everyone votes for Catherines medical answer */
-/* two votes for Bob's history answer, and 1 for Catherine's history answer */
-/* Catherine has 105 medical, 101 history, 100 business.  Bob has 100 medical, 104 history, 100 business */
+    /* add dummy votes data - Darcie, Eugene and Fortraine vote for one answer for each of the three questions */
+    /* answer 1 and 2 are Bob's (medical and history), 3 and 4 are Catherine's (medical and history) */
+    /* everyone votes for Catherines medical answer */
+    /* two votes for Bob's history answer, and 1 for Catherine's history answer */
+    /* Catherine has 105 medical, 101 history, 100 business.  Bob has 100 medical, 104 history, 100 business */
 INSERT INTO votes 
     (vote_date, vote_user, vote_answer)
 VALUES
@@ -70,11 +72,11 @@ VALUES
     (current_timestamp, (SELECT user_id FROM users WHERE user_name='Ingrid'), 2), 
     (current_timestamp, (SELECT user_id FROM users WHERE user_name='Janice'), 3), 
     (current_timestamp, (SELECT user_id FROM users WHERE user_name='Janice'), 2),
-/* Add dummy votes for Bob and Catherine to test the weighted vote system */
-/* 5, 7, 9 are Grant's answers to medical, history and business.  6, 8, 10 are Heather's answers to medical, history, and business */
-/* Medical: Grant's answer should have 100/205, and Heather's answer 105/205*/
-/* History:  Grant's answer should have 104/205 and Heather's answer 101/205 */
-/* Business:  Grant's answer should have 100/200 and Heather's answer 100/200 */
+    /* Add dummy votes for Bob and Catherine to test the weighted vote system */
+    /* 5, 7, 9 are Grant's answers to medical, history and business.  6, 8, 10 are Heather's answers to medical, history, and business */
+    /* Medical: Grant's answer should have 100/205, and Heather's answer 105/205*/
+    /* History:  Grant's answer should have 104/205 and Heather's answer 101/205 */
+    /* Business:  Grant's answer should have 100/200 and Heather's answer 100/200 */
     (current_timestamp, (SELECT user_id FROM users WHERE user_name='Bob'), 5),
     (current_timestamp, (SELECT user_id FROM users WHERE user_name='Catherine'), 6),
     (current_timestamp, (SELECT user_id FROM users WHERE user_name='Bob'), 7),
