db295ab4253899b2784ca9fbbcfd4f3f19743d7d
Thomas <thomasthienbui@gmail.com>
2021-03-23 18:05:23 +0700

    Fixed bug with not calling next()

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type, set-cookie');
     res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
